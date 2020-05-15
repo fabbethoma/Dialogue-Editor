e72e440d6268455991d06198a9fba3bb599b52e3
fabbethoma <55735538+fabbethoma@users.noreply.github.com>
2020-05-15 17:41:23 +0200

    Added styling file for mic recorder

---
+++ src/AudioRecorder/styledRecorder.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+display:flex;
+justify-content: center;
+flex-direction: row;
+border: 1px solid red;
+`;
+
+export const RecButton = styled.button`
+border-radius: 20px;
+background-color: #FF4444;
+border: none;
+// margin-bottom: 30px;
+justify-content: center;
+height: 30px;
+margin-top: 12px;
+
+`;
+
+export const StopButton = styled.button`
+
+border: none;
+border-radius: 20px;
+margin-left: 20px;
+margin-right: 20px;
+font-size: 45px;
+background: none;
+margin-bottom: 12px;
+
+`;

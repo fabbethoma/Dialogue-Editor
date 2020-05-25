acaa2298b352e11103d87dffbaf2fdd139ed4ea3
haiip <Nikita.magnusson@live.se>
2020-05-25 16:32:38 +0200

    New design test

--- src/Page/ExchangeList/index.js
+++ src/Page/ExchangeList/index.js
@@ -1,56 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 // import Form from 'form-js'
-import { withFirebase } from '../../Firebase';
+import { withFirebase } from "../../Firebase";
 
-import * as STYLES from './style'
+import * as STYLES from "./style";
 
 //import * as STYLES from '../styledExchange'
 
-import ExchangeEdit from '../../ExchangeEdit';
+import ExchangeEdit from "../../ExchangeEdit";
 
 class ExchangeList extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       exchanges: null,
-      editExchange: null
-    }
-
+      editExchange: null,
+    };
   }
   toggleEditExchange = (exchangeID = null) => {
-    console.log("tEE ran " + exchangeID)
+    console.log("tEE ran " + exchangeID);
 
-
-
-    this.setState({ editExchange: exchangeID })
-  }
+    this.setState({ editExchange: exchangeID });
+  };
   componentDidMount() {
-
     // this.props.exchanges : {-M7D9_kEGnRWODa19EJS: true, -M7D9kEEmH1H3lQ
 
-    this.props.firebase.exchanges().on('value', snapshot => {
+    this.props.firebase.exchanges().on("value", (snapshot) => {
       const exchangesObj = snapshot.val();
       if (exchangesObj) {
-        const newObj = {}
-
+        const newObj = {};
         Object.values(exchangesObj).filter((item, index) => {
-          if (Object.keys(item.scenarios).includes(this.props.scenarioID) === true) {
-
-            newObj[Object.keys(exchangesObj)[index]] = item
+          if (
+            Object.keys(item.scenarios).includes(this.props.scenarioID) === true
+          ) {
+            newObj[Object.keys(exchangesObj)[index]] = item;
           }
-          return Object.keys(item.scenarios).includes(this.props.scenarioID) === true;
-        })
+          return (
+            Object.keys(item.scenarios).includes(this.props.scenarioID) === true
+          );
+        });
 
-        const sortObj = {}
-        const sortKeys = Object.keys(newObj).sort((a, b) => newObj[a].exchangeOrder > newObj[b].exchangeOrder ? 1 : -1)
+        const sortObj = {};
+        const sortKeys = Object.keys(newObj).sort((a, b) =>
+          newObj[a].exchangeOrder > newObj[b].exchangeOrder ? 1 : -1
+        );
         // console.log(sortKeys)
-        sortKeys.forEach((key, index) => sortObj[Object.keys(newObj)[index]] = newObj[key])
-        let exchangesList = Object.keys(sortObj).map(key => {
+        sortKeys.forEach(
+          (key, index) => (sortObj[Object.keys(newObj)[index]] = newObj[key])
+        );
+        let exchangesList = Object.keys(sortObj).map((key) => {
           // if (validExchanges.includes(key)) {
-          return ({
+          return {
             ...sortObj[key],
             uid: key,
-          })
+          };
           /*
             } else {
                 return null;
@@ -59,8 +61,6 @@ class ExchangeList extends Component {
         });
         this.setState({
           exchanges: exchangesList,
-          keysID: sortKeys
-
         });
       }
 
@@ -74,169 +74,181 @@ class ExchangeList extends Component {
   }
   onChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
-  }
+  };
 
-  handleRemoveClick = index => {
+  handleRemoveClick = (index) => {
     //  this.props.firebase.exchanges(index).remove();
-  }
+  };
 
-  handleEditClick = () => {
-
-  }
+  handleEditClick = () => {};
   duplicateHandle = (event) => {
     event.preventDefault();
-    const keyID = event.target.id // check out element.closest()
+    const keyID = event.target.id; // check out element.closest()
     console.log(keyID);
-    const Obj = this.state.exchanges
+    const Obj = this.state.exchanges;
     const objFilter = Object.keys(Obj).reduce((object, key) => {
       if (key === keyID) {
-        object[key] = Obj[key]
+        object[key] = Obj[key];
       }
-      return object
-    }, [])
-    console.log(objFilter)
-    const copyObj = Object.assign({}, ...objFilter)
-    delete copyObj.uid
-    delete copyObj.audio
-    delete copyObj.audioFileName
-    copyObj.exchangeOrder = Obj.length
-    console.log(copyObj)
-    const ref = this.props.firebase.exchanges().push(copyObj)
-    this.props.firebase.scenario(this.props.scenarioID).child('exchanges').update({ [ref.key]: true })
-  }
+      return object;
+    }, []);
+    console.log(objFilter);
+    const copyObj = Object.assign({}, ...objFilter);
+    delete copyObj.uid;
+    delete copyObj.audio;
+    delete copyObj.audioFileName;
+    copyObj.exchangeOrder = Obj.length;
+    console.log(copyObj);
+    const ref = this.props.firebase.exchanges().push(copyObj);
+    this.props.firebase
+      .scenario(this.props.scenarioID)
+      .child("exchanges")
+      .update({ [ref.key]: true });
+  };
 
   deleteExchange = (event) => {
     event.preventDefault();
-    const keyID = event.target.id// check out element.closest()
-    const getUid = this.props.scenarioID
+    const keyID = event.target.id; // check out element.closest()
+    const getUid = this.props.scenarioID;
     console.log(keyID);
-    const idKey = this.state.keysID[parseInt(keyID)]
-    const Obj = this.state.exchanges
-    const updateObj = []
-
+    const idKey = Object.keys(this.props.exchanges)[keyID];
+    const Obj = this.state.exchanges;
+    const updateObj = [];
     const objFilter = Object.keys(Obj).reduce((object, key) => {
       if (key === keyID) {
-        console.log(key)
-        object[key] = Obj[key]
+        object[key] = Obj[key];
       } else {
-        updateObj.push(Obj[key])
+        updateObj.push(Obj[key]);
       }
-      return object
-    }, [])
-
-    const filtred = Object.assign({}, ...objFilter)
-    const boolean = 'audioFileName' in filtred
-    console.log(filtred)
-    console.log(updateObj)
+      return object;
+    }, []);
+    const filterObj = [];
+    const filtred = Object.assign({}, ...objFilter);
+    const boolean = "audioFileName" in filtred;
+    console.log(filtred);
+    console.log(updateObj);
     if (boolean) {
-      const deleteFileName = filtred.audioFileName
+      const deleteFileName = filtred.audioFileName;
 
-      this.props.firebase.audio().child('audio/' + deleteFileName).delete().then(() => {
-        this.props.firebase.exchange(idKey).remove().then(value => {
-          console.log(value);
-          this.props.firebase.scenarioExchange(getUid).child(`/${idKey}`).remove()
+      this.props.firebase
+        .audio()
+        .child("audio/" + deleteFileName)
+        .delete()
+        .then(() => {
+          this.props.firebase
+            .exchange(idKey)
+            .remove()
+            .then((value) => {
+              console.log(value);
+              this.props.firebase
+                .scenarioExchange(getUid)
+                .child(`/${idKey}`)
+                .remove();
+            });
         })
-      }).then(() => {
-        updateObj.forEach((item, key) => {
-          this.props.firebase.exchanges().child(`/${item.uid}`).update({
-            exchangeOrder: key
-          })
-        })
-      })
+        .then(() => {
+          updateObj.forEach((item, key) => {
+            this.props.firebase.exchanges().child(`/${item.uid}`).update({
+              exchangeOrder: key,
+            });
+          });
+        });
     } else {
-      this.props.firebase.exchange(idKey).remove().then(() => {
-        this.props.firebase.scenarioExchange(getUid).child(`/${idKey}`).remove()
-      }).then(() => {
-        updateObj.forEach((item, key) => {
-          this.props.firebase.exchanges().child(`/${item.uid}`).update({
-            exchangeOrder: key
-          })
+      this.props.firebase
+        .exchange(idKey)
+        .remove()
+        .then(() => {
+          this.props.firebase
+            .scenarioExchange(getUid)
+            .child(`/${idKey}`)
+            .remove();
         })
-      })
+        .then(() => {
+          updateObj.forEach((item, key) => {
+            this.props.firebase.exchanges().child(`/${item.uid}`).update({
+              exchangeOrder: key,
+            });
+          });
+        });
     }
-  }
+  };
 
-  uppOrder = (event) => {
-    event.preventDefault();
-    const idOrder = event.target.id
-    const allArr = []
-    const oldIndex = parseInt(idOrder)
-    const newIndex = oldIndex - 1;
-    const oldArray = this.state.exchanges
-
-    const newArray = this.ChangeArray(oldArray, oldIndex, newIndex)
-    console.log(newArray)
-    const test = []
-    newArray.forEach((exchange, index) => {
-      exchange.exchangeOrder = index
-      test.push(exchange)
-    })
-    console.log(test)
-  }
-
-  downOrder = (event) => {
-    event.preventDefault();
-    const idOrder = event.target.id
-    const oldIndex = parseInt(idOrder)
-    const newIndex = oldIndex + 1;
-    const oldArray = this.state.exchanges
-
-    const newArray = this.ChangeArray(oldArray, oldIndex, newIndex)
-    console.log(newArray)
-    const test = []
-    newArray.forEach((exchange, index) => {
-      exchange.exchangeOrder = index
-      test.push(exchange)
-    })
-    console.log(test)
-  }
-
-  ChangeArray = (arr, old_index, new_index) => {
-    while (old_index < 0) {
-      old_index += arr.length;
-    }
-    while (new_index < 0) {
-      new_index += arr.length;
-    }
-    if (new_index >= arr.length) {
-      var k = new_index - arr.length;
-      while ((k--) + 1) {
-        arr.push(undefined);
-      }
-    }
-    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
-    return arr;
-  }
   render() {
     //const { onSubmit,onChange} = this.props
     const validExchanges = Object.keys(this.props.exchanges);
     return (
       <div>
+        {this.state.exchanges
+          ? this.state.exchanges.map((item, key) => (
+              <STYLES.exchangeDiv key={key}>
+                <STYLES.SpanExchange>
+                  <STYLES.editButton
+                    onClick={() => this.toggleEditExchange(item.uid)}
+                  >
+                    {" "}
+                    EDIT{" "}
+                  </STYLES.editButton>
+                  <STYLES.DuplicateButton
+                    id={key}
+                    onClick={this.duplicateHandle}
+                  >
+                    Duplicate
+                  </STYLES.DuplicateButton>
+                  Role: {item.role}
+                  <STYLES.deleteButton id={key} onClick={this.deleteExchange}>
+                    {" "}
+                    X{" "}
+                  </STYLES.deleteButton>
+                </STYLES.SpanExchange>
+                {/* <STYLES.p>Role: {item.role}</STYLES.p> */}
+                <STYLES.p>
+                  {" "}
+                  <b>Text: </b> {item.text}
+                </STYLES.p>
+                <STYLES.p>
+                  {" "}
+                  <b>Mood: </b> {item.mood}
+                </STYLES.p>
 
-        {this.state.exchanges ? this.state.exchanges.map((item, key) => (
-          <STYLES.exchangeDiv key={key}>
-            <STYLES.SpanExchange><STYLES.editButton onClick={() => this.toggleEditExchange(item.uid)}> EDIT </STYLES.editButton>
-              <button id={key} onClick={this.duplicateHandle} >Duplicate</button>
-                Role: {item.role}
-              <STYLES.deleteButton id={key} onClick={this.deleteExchange}> X </STYLES.deleteButton>
-            </STYLES.SpanExchange>
-            {/* <STYLES.p>Role: {item.role}</STYLES.p> */}
-            <STYLES.p> <b>Text: </b> {item.text}</STYLES.p>
-            <STYLES.p> <b>Mood: </b> {item.mood}</STYLES.p>
-
-            {/* <AudioPlayer url={item.audio} /> */}
-            {/* <STYLES.deleteButton onClick={this.deleteExchange}> X </STYLES.deleteButton> */}
-            {key > 0 ? <button onClick={this.uppOrder} style={{ width: '80px' }} id={key}>UppOrder</button> : null}
-            {key < this.state.exchanges.length - 1 ? <button onClick={this.downOrder} style={{ width: '80px' }} id={key}>DownOrder</button> : null}
-
-            <h5 style={{ paddingLeft: '10px' }}> Exchange key {key + 1} </h5>
-          </STYLES.exchangeDiv>
-        )) : null}
-        {this.state.editExchange && <ExchangeEdit key={this.state.editExchange} exchange={this.state.exchanges.find(ex => ex.uid === this.state.editExchange)} exchangeID={this.state.editExchange} toggleEditExchange={this.toggleEditExchange} />}
-
-
+                {/* <AudioPlayer url={item.audio} /> */}
+                {/* <STYLES.deleteButton onClick={this.deleteExchange}> X </STYLES.deleteButton> */}
+                {key > 0 ? (
+                  <button
+                    onClick={this.uppOrder}
+                    style={{ width: "80px" }}
+                    id={key}
+                  >
+                    UppOrder
+                  </button>
+                ) : null}
+                {key < this.state.exchanges.length - 1 ? (
+                  <button
+                    onClick={this.downOrder}
+                    style={{ width: "80px" }}
+                    id={key}
+                  >
+                    DownOrder
+                  </button>
+                ) : null}
+                <h5 style={{ paddingLeft: "10px" }}>
+                  {" "}
+                  Exchange key {key + 1}{" "}
+                </h5>
+              </STYLES.exchangeDiv>
+            ))
+          : null}
+        {this.state.editExchange && (
+          <ExchangeEdit
+            key={this.state.editExchange}
+            exchange={this.state.exchanges.find(
+              (ex) => ex.uid === this.state.editExchange
+            )}
+            exchangeID={this.state.editExchange}
+            toggleEditExchange={this.toggleEditExchange}
+          />
+        )}
       </div>
-    )
+    );
   }
 }
 
--- src/Page/ExchangeList/style.js
+++ src/Page/ExchangeList/style.js
@@ -1,37 +1,33 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const exchangeDiv = styled.div`
+  font-family: "Montserrat", sans-serif;
 
-font-family: 'Montserrat', sans-serif;
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  margin-top: 10px;
 
-display: flex;
-flex-direction: column;
-margin: 0;
-margin-top: 10px;
+  width: 40%;
 
-width: 40%;
-
-
-:nth-child(even) {
-    border: 2px solid #765CF6;
+  :nth-child(even) {
+    border: 2px solid #765cf6;
     float: right;
     margin: 10px;
     margin-top: 20px;
-    border-right: 6px solid #765CF6;
-    border-top: 10px solid #765CF6;
+    border-right: 6px solid #765cf6;
+    border-top: 10px solid #765cf6;
     border-radius: 100px 100px 100px 100px;
     -moz-border-radius: 100px 100px 100px 100px;
     -webkit-border-radius: 100px 20px 20px 20px;
-    -webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
-     -moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
-     box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
-    
-
+    -webkit-box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+    -moz-box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+    box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
 
     p {
       padding-left: 10px;
     }
-    h4{
+    h4 {
       padding-left: 10px;
     }
 
@@ -39,69 +35,69 @@ width: 40%;
       margin-left: 40px;
       margin-bottom: 5px;
     }
-}
+  }
 
-:nth-child(odd) {
-    border: 2px solid #FF6858;
+  :nth-child(odd) {
+    border: 2px solid #ff6858;
     justify-content: float-right;
     float: left;
     flex-direction: column;
     margin-left: 10px;
     padding-bottom: 5px;
     padding-left: 5px;
-    border-left: 6px solid #FF6858;
-    border-top: 10px solid #FF6858;
+    border-left: 6px solid #ff6858;
+    border-top: 10px solid #ff6858;
     border-radius: 100px 100px 100px 100px;
     -moz-border-radius: 100px 100px 100px 100px;
     -webkit-border-radius: 20px 20px 100px 20px;
-    -webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
-     -moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
-     box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
-}
-
+    -webkit-box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+    -moz-box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+    box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+  }
 `;
 
 export const deleteButton = styled.button`
-
-    border-radius: 20px;
-    background-color: #FF6858;
-    border: none;
-    width: 5%;
-    font-family: 'Montserrat', sans-serif;
-    font-weight: bold;
-    color: white;
-    float: right;
-    margin-right: 10px;
-
-
+  border-radius: 20px;
+  background-color: #ff6858;
+  border: none;
+  width: 5%;
+  font-family: "Montserrat", sans-serif;
+  font-weight: bold;
+  color: white;
+  float: right;
+  margin-right: 10px;
 `;
 
-
+export const DuplicateButton = styled.button`
+  border-radius: 20px;
+  background-color: #59d87f;
+  width: 15%;
+  margin-left: 5px;
+  border: none;
+  font-family: "Montserrat", sans-serif;
+  font-weight: bold;
+  margin-right: 100px;
+`;
 
 export const editButton = styled.button`
-
-    border-radius: 20px;
-    background-color: #FFD03A;
-    width: 10%;
-    margin-left: 5px;
-    border: none;
-    font-family: 'Montserrat', sans-serif;
-    font-weight: bold;
-    margin-right: 100px;
-    
+  border-radius: 20px;
+  background-color: #ffd03a;
+  width: 10%;
+  margin-left: 5px;
+  border: none;
+  font-family: "Montserrat", sans-serif;
+  font-weight: bold;
+  margin-right: 100px;
 `;
 
 export const p = styled.p`
-  border-bottom: 2px dotted #D9D9D9;
-
+  border-bottom: 2px dotted #d9d9d9;
 `;
 
 export const TextArea = styled.textarea`
-
-height: 30px;
-
+  height: 30px;
 `;
 
 export const SpanExchange = styled.span`
-margin : 21.3px 0;
-`
+  margin: 21.3px 0;
+`;
